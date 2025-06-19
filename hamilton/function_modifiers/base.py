84b76ac0a54a3f94a9c0070b5158a59cd57bdd5e
zilto <68975210+zilto@users.noreply.github.com>
2025-06-19 09:33:29 +0200

    disabled autoloading of plugins; this will make tests fail

--- hamilton/function_modifiers/base.py
+++ hamilton/function_modifiers/base.py
@@ -20,7 +20,9 @@ if not registry.INITIALIZED:
     # Trigger load of extensions here because decorators are the only thing that use the registry
     # right now. Side note: ray serializes things weirdly, so we need to do this here rather than in
     # in the other choice of hamilton/base.py.
-    registry.initialize()
+    # NOTE commented out to disable autoloading
+    # registry.initialize()
+    pass
 
 
 def sanitize_function_name(name: str) -> str:
